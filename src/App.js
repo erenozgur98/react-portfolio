dd7c9da87f4c5c005731a980147df742a46783eb
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-13 19:45:52 -0400

    created functionality inside those new files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './styles/style.css';
 import HomePage from './pages/HomePage';
-
+import About from './pages/About';
+import Projects from './pages/Projects';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+      <Navbar />
+      <Route path="/" exact page={HomePage} />
+      <Route path="/about" exact page={About} />
+      <Route path="/projects" exact page={Projects} />
+    </Router>
   );
 }
 
