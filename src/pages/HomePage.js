864611d7f9569d59889b95dec4b57fa748174d9d
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-13 19:15:31 -0400

    created style.css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Snake from 'react-simple-snake';
 
 const HomePage = () => {
     return (
         <>
-         <h1>Hello World</h1>   
+         <Snake />   
         </>
     )
 }
