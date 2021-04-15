681cec300f9bfd332b96c5c0432afce697728ac6
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-15 19:37:04 -0400

    added Switch around Routes in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './components/Home/Home';
 // import Snake from 'react-simple-snake';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Games from './components/Games/Games';
@@ -13,10 +13,12 @@ function App() {
     <>
       <Router>
         <Navbar />
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/games" component={Games} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/games" component={Games} />
+        </Switch>
       </Router>
       <Footer />
     </>
