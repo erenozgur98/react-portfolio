74bd2019b0ec2c3771fdc1d50b1f0a376af82b13
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-14 20:49:23 -0400

    deleted the annoying > inside the html page, changed the title name, testing the typewritter effect looks awesome!

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import Type from './Type';
-import '../styles/style.css';
+import '../../styles/style.css';
 
 const HomePage = () => {
     return (
