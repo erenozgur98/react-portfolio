54965b35c792df9afd7e8185213b8cbd0ade8598
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-15 20:26:21 -0400

    still deciding what to do with the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,20 +8,18 @@ const HomePage = () => {
     return (
         <section>
             <Container fluid className="home-section" id="home">
-                <Particle />
-                <Container className="home-content">
-                    <Row>
-                        <Col md={7} className="home-header">
-                            <h2 className="heading-name">
-                                Hey there! I'm Yigit Eren Ozgur
-                            </h2>
-                            <div className="type" style={{ textAlign: 'center', padding: "24px", marginBottom: "24px" }}>
-                                <Type />
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+                <Row>
+                    <Col md={7} className="home-header">
+                        {/* <h2 className="heading-name">
+                            Hey there! I'm Yigit Eren Ozgur
+                            </h2> */}
+                        <div className="type">
+                            <Type />
+                        </div>
+                    </Col>
+                </Row>
             </Container>
+            <Particle />
         </section>
     )
 }
