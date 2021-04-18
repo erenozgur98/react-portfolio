56a4d52a36c71ca1d1d7163e59186eae259fc50d
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-18 12:29:08 -0400

    added linkedin and github links to footer, added fa icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import '../../styles/style.css';
 
 function Footer() {
   return (
-    <footer className="footer text-center">
-        Yigit Eren Ozgur | Portfolio | 2021
-    </footer>
+    <div className="footer">
+      Yigit Eren Ozgur | 2021
+     <a
+        href="https://www.linkedin.com/in/yigit-eren-ozgur/"
+        style={{ color: "#fff" }}
+     >
+       <i className="fab fa-linkedin-in"></i>
+     </a>
+     <a
+        href="github.com/erenozgur98"
+        style={{ color: "#fff" }}
+     >
+       <i className="fab fa-github"></i>
+     </a>
+    </div >
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,44 +5,34 @@ import '../../styles/style.css';
 function NavBar() {
     return (
         <div className="navigation">
-            <nav className="navbar navbar-expand-lg navbar-dark">
+            <nav className="navbar navbar-expand-lg">
                 <Link className="navbar-brand" to="/">
-                    erenozgur
-            </Link>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link
-                            to="/home"
-                            className={window.location.pathname === "/home" ? "nav-link active" : "nav-link"}
-                        >
-                            <i className="fas fa-home"></i>Home
+                    <img src="" className="logo" alt="logo" />
                 </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/about"
-                            className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
-                        >
-                            <i className="fas fa-user"></i>About Me
+                <Link
+                    to="/home"
+                    className={window.location.pathname === "/home" ? "nav-link active" : "nav-link"}
+                >
+                    <i className="fas fa-home"></i>Home
                 </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/projects"
-                            className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}
-                        >
-                            Projects
+                <Link
+                    to="/about"
+                    className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+                >
+                    <i className="fas fa-user"></i>About Me
                 </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/games"
-                            className={window.location.pathname === "/games" ? "nav-link active" : "nav-link"}
-                        >
-                            Games
+                <Link
+                    to="/projects"
+                    className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}
+                >
+                    <i class="fas fa-code"></i>Projects
                 </Link>
-                    </li>
-                </ul>
+                <Link
+                    to="/games"
+                    className={window.location.pathname === "/games" ? "nav-link active" : "nav-link"}
+                >
+                    <i class="fas fa-dice"></i>Games
+                </Link>
             </nav>
         </div>
     );
