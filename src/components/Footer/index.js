06282e7d406b1f3859a1cd47e354650637c762fb
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-18 12:36:50 -0400

    added more styles to the icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import '../../styles/style.css';
 
 function Footer() {
@@ -7,14 +6,20 @@ function Footer() {
     <div className="footer">
       Yigit Eren Ozgur | 2021
      <a
+        href="mailto:yigiterenozgur@outlook.com"
+        style={{ color: "#D1920D" }}
+     >
+       <i class="fas fa-envelope"></i>
+     </a>
+     <a
         href="https://www.linkedin.com/in/yigit-eren-ozgur/"
-        style={{ color: "#fff" }}
+        style={{ color: "#D1920D" }}
      >
        <i className="fab fa-linkedin-in"></i>
      </a>
      <a
         href="github.com/erenozgur98"
-        style={{ color: "#fff" }}
+        style={{ color: "#D1920D" }}
      >
        <i className="fab fa-github"></i>
      </a>
