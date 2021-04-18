30170db79233a92e3404dbc52264d9432f419823
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-18 12:46:10 -0400

    added more style for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,25 +4,31 @@ import '../../styles/style.css';
 function Footer() {
   return (
     <div className="footer">
-      Yigit Eren Ozgur | 2021
-     <a
+        Yigit Eren Ozgur | 2021
+      <a
+        href="tel:2019930466"
+        style={{ color: "#D1920D" }}
+      >
+        <i class="fas fa-phone"></i>
+      </a>
+      <a
         href="mailto:yigiterenozgur@outlook.com"
         style={{ color: "#D1920D" }}
-     >
-       <i class="fas fa-envelope"></i>
-     </a>
-     <a
+      >
+        <i class="fas fa-envelope"></i>
+      </a>
+      <a
         href="https://www.linkedin.com/in/yigit-eren-ozgur/"
         style={{ color: "#D1920D" }}
-     >
-       <i className="fab fa-linkedin-in"></i>
-     </a>
-     <a
+      >
+        <i className="fab fa-linkedin-in"></i>
+      </a>
+      <a
         href="github.com/erenozgur98"
         style={{ color: "#D1920D" }}
-     >
-       <i className="fab fa-github"></i>
-     </a>
+      >
+        <i className="fab fa-github"></i>
+      </a>
     </div >
   );
 }
