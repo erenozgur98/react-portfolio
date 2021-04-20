e13f97d1c7ad17b107ad29b74ab9c0da584ffbae
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-19 21:10:06 -0400

    added classNames for aboutMe

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,11 +4,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 function About() {
     return (
         <section>
-            <Container fluid className="" id="about">
+            <Container className="about-me" id="about">
                 <Row>
                     <Col>
-                        <h2 style={{ textAlign: 'center' }}>About Me</h2>
-                        <p className="small-about-me">Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience</p>
+                        <h2 className="about-me-h2" style={{ textAlign: 'center' }}>About Me</h2>
+                        <p
+                            style={{ textAlign: 'center' }}
+                            className="small-about-me"
+                        >
+                            Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience
+                        </p>
                     </Col>
                 </Row>
             </Container>
