fc681263bc6d71f1a91ab7818c48eea29dd8ce94
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-19 21:08:50 -0400

    added more content inside the about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,8 @@ function About() {
             <Container fluid className="" id="about">
                 <Row>
                     <Col>
-                        <h2 style={{ textAlign: 'center' }}>This is where About Me Goes</h2>
+                        <h2 style={{ textAlign: 'center' }}>About Me</h2>
+                        <p className="small-about-me">Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience</p>
                     </Col>
                 </Row>
             </Container>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,7 +11,6 @@ const HomePage = () => {
                 <h2 className="heading-name">
                     Hey there! I'm <span class="name">Yigit Eren Ozgur</span>
                 </h2>
-                <p className="small-about-me">Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience</p>
             </Container>
             <Container className="picture">
                 <img src={pic} className="profile-picture" alt="profilePicture" />
