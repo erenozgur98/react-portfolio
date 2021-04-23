8e7fbe5a186322b8d074a004bb4b201767dcddec
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-22 23:09:56 -0400

    removed the links from the home page as well, will ask tomorrow why isn't this working

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Type from './Type';
 import pic from './pic.jpeg'
 import './style.css';
@@ -12,14 +13,13 @@ const HomePage = () => {
                     Hey there! I'm <span class="name">Yigit Eren Ozgur</span>
                 </h2>
                 <p className="home-about-me">
-                    
                 </p>
-            </Container>
-            <Container className="picture">
-                <img src={pic} className="profile-picture" alt="profilePicture" />
                 <div className="type">
                     <Type />
                 </div>
+            </Container>
+            <Container className="picture">
+                <img src={pic} className="profile-picture" alt="profilePicture" />
             </Container>
         </section>
     )
