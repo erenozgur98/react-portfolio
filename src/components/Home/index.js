51ca065c75e564e17e19d8bd3e006b0324fbf7c5
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-29 17:08:58 -0400

    got rid of unnecessary links

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import Type from './Type';
 import pic from './pic.jpeg'
 import './style.css';
