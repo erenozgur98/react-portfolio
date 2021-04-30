4f9e65358082358c287dc280070c85b5648dbe56
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-29 21:53:24 -0400

    adding pictures of the projects

--- src/mydata.js
+++ src/mydata.js
@@ -4,37 +4,37 @@ projects: [
     {
         title: 'Project One',
         description: '',
-        imageSrc: '',
+        imageSrc: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60',
         url: ''
     },
     {
         title: 'Project Two',
         description: '',
-        imageSrc: '',
+        imageSrc: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60',
         url: ''
     },
     {
         title: 'Project Three',
         description: '',
-        imageSrc: '',
+        imageSrc: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60',
         url: ''
     },
     {
         title: 'Project Four',
         description: '',
-        imageSrc: '',
+        imageSrc: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60',
         url: ''
     },
     {
         title: 'Project Five',
         description: '',
-        imageSrc: '',
+        imageSrc: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60',
         url: ''
     },
     {
         title: 'Project Six',
         description: '',
-        imageSrc: '',
+        imageSrc: 'https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60',
         url: ''
     }
 ]
