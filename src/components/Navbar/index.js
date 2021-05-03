14e5d929831a50903cb42a6ae50721dd2963744a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 22:13:23 -0400

    adding skills and adding styles to the skills

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,74 +5,72 @@ import logo from '../Navbar/logo.png'
 
 function NavBar() {
     return (
-        <>
-        </>
-        // <div className="navigation">
-        //     <nav className="navbar">
-        //         <Link className="navbar-brand" to="/">
-        //             <img src={logo} className="logo" alt="logo" />
-        //         </Link>
-        //         <Link
-        //             to="/home"
-        //             className="nav-link"
-        //         >
-        //             <div className="navLinks">
-        //                 <i
-        //                     style={{ paddingRight: "8px", color: "#D1920D" }}
-        //                     className="fas fa-home"></i>
-        //                 Home
-        //             </div>
-        //         </Link>
-        //         <Link
-        //             to="/about"
-        //             className="nav-link"
-        //         >
-        //             <div className="navLinks">
-        //                 <i
-        //                     style={{ paddingRight: "8px", color: "#D1920D" }}
-        //                     className="fas fa-user"></i>
-        //                 About Me
-        //             </div>
-        //         </Link>
-        //         <Link
-        //             to="/projects"
-        //             className="nav-link"
-        //         >
-        //             <div className="navLinks">
-        //                 <i
-        //                     style={{ paddingRight: "8px", color: "#D1920D" }}
-        //                     class="fas fa-code"></i>
-        //                 Projects
-        //             </div>
-        //         </Link>
-        //         {/* <Link
-        //             to="/games"
-        //             className="nav-link"
-        //         >
-        //             <div className="navLinks">
-        //                 <i
-        //                     style={{ paddingRight: "8px", color: "#D1920D" }}
-        //                     class="fas fa-dice"></i>
-        //                     Games
+        <div className="navigation">
+            <nav className="navbar">
+                <Link className="navbar-brand" to="/">
+                    <img src={logo} className="logo" alt="logo" />
+                </Link>
+                <Link
+                    to="/home"
+                    className="nav-link"
+                >
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            className="fas fa-home"></i>
+                        Home
+                    </div>
+                </Link>
+                <Link
+                    to="/about"
+                    className="nav-link"
+                >
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            className="fas fa-user"></i>
+                        About Me
+                    </div>
+                </Link>
+                <Link
+                    to="/projects"
+                    className="nav-link"
+                >
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            class="fas fa-code"></i>
+                        Projects
+                    </div>
+                </Link>
+                {/* <Link
+                    to="/games"
+                    className="nav-link"
+                >
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            class="fas fa-dice"></i>
+                            Games
 
-        //             </div>
-        //         </Link> */}
-        //         <a
-        //             href={"https://docs.google.com/document/d/1Fk0PF1XHbvAywMvCG-2oCISqmK-Ttk3C2h_YBoPzKW8/edit"}
-        //             target="_blank"
-        //             rel="noreferrer"
-        //             className="nav-link"
-        //         >
-        //             <div className="navLinks">
-        //                 <i
-        //                     style={{ paddingRight: "8px", color: "#D1920D" }}
-        //                     class="far fa-file-alt"></i>
-        //                     Resume
+                    </div>
+                </Link> */}
+                <a
+                    href={"https://docs.google.com/document/d/1Fk0PF1XHbvAywMvCG-2oCISqmK-Ttk3C2h_YBoPzKW8/edit"}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="nav-link"
+                >
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            class="far fa-file-alt"></i>
+                            Resume
 
-        //             </div>
-        //         </a>
-        //     </nav>
-        // </div>
+                    </div>
+                </a>
+            </nav>
+        </div>
     );
 }
 
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,7 +1,44 @@
+import css from '../../img/css.svg';
+import code from '../../img/code.svg';
+import html from '../../img/html.svg';
+import react from '../../img/react.svg';
+import javascript from '../../img/javascript.svg';
+import jquery from '../../img/jquery.jpg';
+import node from '../../img/node.png';
+import mysql from '../../img/mysql.png';
+
 const Skills = [
     {
-        img: '',
-        description: ''
+        img: css,
+        description: 'css'
+    },
+    {
+        img: code,
+        description: 'code'
+    },
+    {
+        img: html,
+        description: 'html'
+    },
+    {
+        img: react,
+        description: 'react'
+    },
+    {
+        img: jquery,
+        description: 'jquery'
+    },
+    {
+        img: javascript,
+        description: 'javascript'
+    },
+    {
+        img: node,
+        description: 'node'
+    },
+    {
+        img: mysql,
+        description: 'mysql'
     }
 ]
 
