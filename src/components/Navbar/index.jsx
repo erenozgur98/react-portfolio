8ef14180bf5398b08c38ef1f8bc4fa08f5eb4135
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-09 22:08:05 -0400

    nav styles are done

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,41 +2,51 @@ import React from 'react'
 import { Navbar, Nav, NavbarBrand } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../Navbar/logo.png'
+import './style.css';
 
 function NavBar() {
     return (
         <Navbar collapseOnSelect expand='lg' bg='transparent' variant='dark' fixed='top'>
-            <NavbarBrand className='navbar-brand' to='/'>
+            <NavbarBrand as={Link} className='navbar-brand' to='/'>
                 <img src={logo} className='logo' alt='logo' />
             </NavbarBrand>
             <Navbar.Toggle aria-controls='responsive-navbar-nav' />
             <Navbar.Collapse id='responsive-navbar-nav' />
             <Nav className='ml-auto navbar'>
                 <Nav.Link as={Link} to='/' href='/' className='nav-link'>
-                    <i
-                        style={{ paddingRight: "8px", color: "#D1920D" }}
-                        className="fas fa-home"></i>
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            className="fas fa-home"></i>
                         Home
+                        </div>
                 </Nav.Link>
                 <Nav.Link as={Link} to='/about' href='/about' className='nav-link'>
-                    <i
-                        style={{ paddingRight: "8px", color: "#D1920D" }}
-                        className="fas fa-user"></i>
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            className="fas fa-user"></i>
                         About
+                        </div>
                 </Nav.Link>
                 <Nav.Link as={Link} to='/projects' href='/projects' className='nav-link'>
-                    <i
-                        style={{ paddingRight: "8px", color: "#D1920D" }}
-                        className="fas fa-code"></i>
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            className="fas fa-code"></i>
                         Projects
+                        </div>
                 </Nav.Link>
                 <Nav.Link
                     href={'https://docs.google.com/document/d/1FFk85d-l13BikC6rqBUw_uN6JEtmDJ17f0Cq9ZhaK7g/edit'}
-                    className='nav-link'>
-                    <i
-                        style={{ paddingRight: "8px", color: "#D1920D" }}
-                        className="far fa-file-alt"></i>
+                    className='nav-link'
+                >
+                    <div className="navLinks">
+                        <i
+                            style={{ paddingRight: "8px", color: "#D1920D" }}
+                            className="far fa-file-alt"></i>
                         Resume
+                        </div>
                 </Nav.Link>
             </Nav>
         </Navbar>
