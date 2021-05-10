986b5be36567f1d29bea02e1dfcd7ab697ee7c48
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-09 21:42:24 -0400

    re structring the folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './styles/style.css';
 import HomePage from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Particle from './components/Particle';
@@ -11,15 +10,6 @@ function App() {
   return (
     <>
       <Router>
-        <Particle />
-        <Navbar />
-        <div className="main">
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/home" component={HomePage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-        </div>
-        <Footer />
       </Router>
     </>
   );
