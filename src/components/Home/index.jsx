5620fa17bda2025fd29a00bbf025b464c872f499
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-10 23:39:03 -0400

    added home styles

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -12,24 +12,24 @@ function Home() {
                     <h2 className="heading-name">
                         Hey there! I'm <span className='name'>Yigit Eren Ozgur</span>
                     </h2>
+                </Fade>
+                <Fade bottom cascade>
                     <div className="type">
                         <Type />
                     </div>
                     <p className="home-about-me">
                         Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience.
                     </p>
-                    {/* underscore about me here part */}
                     <a href='/about' className='about-link'>
                         You can read more about me here
                     </a>
                 </Fade>
             </Container>
             <Container className='contact-section'>
-                {/* style the links to contact me */}
                 <Fade bottom cascade>
-                    <h2 className='contact'>
-                        Contact
-                    </h2>
+                    <h2><span className='contact'>Contact</span></h2>
+                </Fade>
+                <Fade bottom cascade>
                     <div>
                         <a
                             href={'mailto:yigiterenozgur@outlook.com'}
@@ -43,6 +43,7 @@ function Home() {
                             href={'mailto:yigiterenozgur@outlook.com'}
                             target='_blank'
                             rel='noreferrer'
+                            class='real-link'
                         >
                             yigiterenozgur@outlook.com
                         </a>
@@ -60,7 +61,7 @@ function Home() {
                             href={'https://www.linkedin.com/in/yigit-eren-ozgur/'}
                             target='_blank'
                             rel='noreferrer'
-                            class='footer-link'
+                            class='real-link'
                         >
                             linkedin.com/in/yigit-eren-ozgur/
                         </a>
@@ -78,7 +79,7 @@ function Home() {
                             href={'https://github.com/erenozgur98'}
                             target='_blank'
                             rel='noreferrer'
-                            className='footer-link'
+                            class='real-link'
                         >
                             github.com/erenozgur98
                         </a>
