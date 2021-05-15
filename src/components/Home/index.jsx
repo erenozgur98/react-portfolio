eca8e67752884acb425bdbe2e1b9daf4ad84de6a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-15 00:30:19 -0400

    fixed small bug

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
                             <Type />
                         </div>
                         <p className="home-about-me">
-                            Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience.
+                            Full-stack web developer leveraging customer service background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of North Carolina at Charlotte, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience.
                         </p>
                         <p className='about-link-p'>
                             Read more about me <a href='/about'><span className='about-link'>here</span></a>
