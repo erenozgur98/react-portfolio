6ec94b5c90bfa62052daa0e2c7ef6e3587370152
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-07-07 20:46:40 -0400

    added new projects for the projects page

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -39,6 +39,46 @@ const Projects = [
         github: 'https://github.com/erenozgur98/avengers-quiz',
         
     },
+    {
+        title: 'Note Taker',
+        description: 'Simple app to take notes',
+        image: '/images/note-taker.jpg',
+        url: 'https://boiling-lake-44798.herokuapp.com/',
+        github: 'https://github.com/erenozgur98/note-taker',
+        
+    },
+    {
+        title: 'Weather Dashboard',
+        description: 'Simple Weather App to see the next 5 days of forecast',
+        image: '/images/weather-dashboard.jpg',
+        url: 'https://erenozgur98.github.io/weather-dashboard/',
+        github: 'https://github.com/erenozgur98/weather-dashboard',
+        
+    },
+    {
+        title: 'Workout Tracker',
+        description: 'This is a Workout Tracker! Keep track of your workouts!',
+        image: '/images/workout.jpg',
+        url: 'https://eren-workout-tracker-001.herokuapp.com/?id=60676157da7b640280226f19',
+        github: 'https://github.com/erenozgur98/workout-tracker',
+        
+    },
+    {
+        title: 'Password Generator',
+        description: 'No more thinking of a new password, this app will create the password you need!',
+        image: '/images/password-generator.jpg',
+        url: 'https://erenozgur98.github.io/password-generator/',
+        github: 'https://github.com/erenozgur98/password-generator',
+        
+    },
+    {
+        title: 'Employee Directory',
+        description: "It's an employee directory that helps you keep track of your employees where you can sort them, filter them and see their information.",
+        image: '/images/ss.jpg',
+        url: 'https://erenozgur98.github.io/employee-directory-react/',
+        github: 'https://github.com/erenozgur98/employee-directory-react',
+        
+    },
 ]
 
 export default Projects;
