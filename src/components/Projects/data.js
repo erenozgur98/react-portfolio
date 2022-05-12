41cdabfdd9f70c0e90611144b8b1e4f99b956a3c
Yiğit Eren Özgür <yigiterenozgur@outlook.com>
2022-05-12 15:57:25 -0400

    May 2022 updates

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,35 +1,19 @@
 const Projects = [
     {
-        title: 'Fresh Air Finder',
-        description: 'National Park Locator which shows all the parks in the state you choose.',
-        image: '/images/fresh-air-finder.jpg',
-        url: 'https://dylanjacobb.github.io/fresh-air-finder/',
-        github: 'https://github.com/erenozgur98/fresh-air-finder',
-        
-    },
-    {
-        title: 'cuPets',
-        description: 'An app foryou and your pet to make new friends!',
-        image: '/images/cupets.jpg',
-        url: 'https://ancient-dawn-51222.herokuapp.com/home',
-        github: 'https://github.com/erenozgur98/cuPets',
-        
-    },
-    {
-        title: 'Memento Multiverse',
-        description: 'Do you love collecting TV merch? Memento Multiverse will be the most fun and funny place to collect memorabilia on the web! ',
-        image: '/images/memento.jpg',
-        url: 'https://memento-multiverse.herokuapp.com/',
-        github: 'https://github.com/erenozgur98/Memento-Multiverse',
-        
-    },
-    {
         title: 'Memorabilia Multiverse',
-        description: 'Re-make of Memento Multiverse, with bootstrap and simpler.',
+        description: 'Do you love collecting TV merch? Memorabilia Multiverse will be the most fun and funny place to collect memorabilia on the web!',
         image: '/images/memorabilia.jpg',
         url: 'https://memorabilia-multiverse.herokuapp.com/',
         github: 'https://github.com/erenozgur98/Memorabilia-Multiverse',
-        
+
+    },
+    {
+        title: 'Fresh Air Finder',
+        description: 'National Park Locator which shows all the parks in the state you choose.',
+        image: '/images/fresh-air-finder.jpg',
+        url: 'https://erenozgur98.github.io/fresh-air-finder/',
+        github: 'https://github.com/erenozgur98/fresh-air-finder',
+
     },
     {
         title: 'Avengers Quiz',
@@ -37,7 +21,7 @@ const Projects = [
         image: '/images/avengers.jpg',
         url: 'https://erenozgur98.github.io/avengers-quiz/',
         github: 'https://github.com/erenozgur98/avengers-quiz',
-        
+
     },
     {
         title: 'Note Taker',
@@ -45,7 +29,7 @@ const Projects = [
         image: '/images/note-taker.jpg',
         url: 'https://boiling-lake-44798.herokuapp.com/',
         github: 'https://github.com/erenozgur98/note-taker',
-        
+
     },
     {
         title: 'Weather Dashboard',
@@ -53,7 +37,7 @@ const Projects = [
         image: '/images/weather-dashboard.jpg',
         url: 'https://erenozgur98.github.io/weather-dashboard/',
         github: 'https://github.com/erenozgur98/weather-dashboard',
-        
+
     },
     {
         title: 'Workout Tracker',
@@ -61,7 +45,7 @@ const Projects = [
         image: '/images/workout.jpg',
         url: 'https://eren-workout-tracker-001.herokuapp.com/?id=60676157da7b640280226f19',
         github: 'https://github.com/erenozgur98/workout-tracker',
-        
+
     },
     {
         title: 'Password Generator',
@@ -69,15 +53,7 @@ const Projects = [
         image: '/images/password-generator.jpg',
         url: 'https://erenozgur98.github.io/password-generator/',
         github: 'https://github.com/erenozgur98/password-generator',
-        
-    },
-    {
-        title: 'Employee Directory',
-        description: "It's an employee directory that helps you keep track of your employees where you can sort them, filter them and see their information.",
-        image: '/images/ss.jpg',
-        url: 'https://erenozgur98.github.io/employee-directory-react/',
-        github: 'https://github.com/erenozgur98/employee-directory-react',
-        
+
     },
 ]
 
